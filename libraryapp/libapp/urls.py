0dbe3c7d69b62fcf5070a5f464d03205864adcb4
Mohammad <m.shirzady44@gmail.com>
2025-01-01 23:26:46 +0330

    signup & login functions are created

--- libraryapp/libapp/urls.py
+++ libraryapp/libapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from libapp.views import home,login
+from libapp import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('login/', login, name='login'),
+    path('',views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('boroow/<str:borrow>',views.borrow,name='borrow')
 ]
--- libraryapp/libapp/views.py
+++ libraryapp/libapp/views.py
@@ -1,53 +1,103 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate ,login
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
-    return HttpResponse('')
-@csrf_exempt
+    return HttpResponse('salam')
+
+
+
+
 #request={"status" : "login/signup", 'username':'UN', password:'PW', 'fname' : 'fname', 'lname': 'lname', 'email':'email', }
 def login(request):
-    if request.POST.get('status') == 'login':
+    print(request.POST.get)
+    if request.method == 'GET':
+        context = {
+            'page' : 'login',
+            'st' : 'notLogin',
+            'msg' : ""
+        }
+        return render(request,'libapp/login.html',context)
+    elif request.POST.get('login'):
         username = request.POST.get('username')
         password = request.POST.get('password')
         usr = authenticate(request,username=username,password=password)
         if usr:
             request.session['user_id'] = User.objects.get(username=username).id
+            
             context = {
                 'page': 'login',
                 'st' : 'loggedin',
                 'msg' : "You're logged in."
             }
-            return HttpResponse("You're logged in.")
-            #return render(request,context)
+            #return HttpResponse("You're logged in.")
+            return render(request,'libapp/login.html',context)
         else:
             context = {
                 'page' : 'login',
                 'st' : 'not login',
                 'msg' : "Your Username and Password didn't match."
             }
-            return HttpResponse("Your Username and Password didn't match.")
-            #return render(request,context)
-    elif request.POST.get('status') == 'signup':
+            #return HttpResponse("Your Username and Password didn't match.")
+            return render(request,'libapp/login.html',context)
+    return HttpResponse('')
+def signup(request):
+    if request.method == 'POST':
         ##TODO  use while to ask agin when some field is empty
         Fname = request.POST.get('Fname')
-        lname = request.POST.get('Lname')
+        Lname = request.POST.get('Lname')
         email = request.POST.get('email')
         username = request.POST.get('username')
         password = request.POST.get('password')
         if not User.objects.filter(username=username):
             ##TODO  send Email and verifying email address
-            newusr = User.objects.create_user(username=username, email=email,password=password)
             newusr.first_name = Fname
-            newusr.last_name = lname
+            newusr.last_name = Lname
+            newusr = User.objects.create_user(lastnname=Lname,firstname=Fname ,username=username, email=email,password=password)
             newusr.save()
-            return HttpResponse("You're signedup.")
+            context = {
+                'page' : 'signup',
+                'st' : 'notLogin',
+                'msg' : "your account is created",
+            }
+        else: 
+            context = {
+                'page' : 'signup',
+                'st' : 'notLogin',
+                'msg' : "your username is not available"
+            }
 
-    elif request.POST.get('test')=='test':
-        if request.user.is_authenticated:
-            return HttpResponse('true!')
-        else:
-            return HttpResponse(request.user)
+        return render(request,'libapp/signup.html',context)
+    else:
+        context = {
+            'page' : 'signup',
+            'st' : 'notLogin',
+            'msg' : ""
+        }
+        return render(request,'libapp/signup.html',context)
+
+#    elif request.POST.get('test')=='test':
+#        if request.user.is_authenticated:
+#            return HttpResponse('true!')
+#        else:
+#            return HttpResponse(request.user)
+
+def borrow(request,borrow):
+    if borrow == 'borrow':
+        pass
+    elif borrow == 'return':
+        pass
+    elif borrow =='':
+        if request.method == 'GET':
+            username = request.user.username
+            context = {
+                'status' : 'get',
+                'username': username,
+            }
+            return render(request, context)
+        elif request.method == 'POST':
+            pass
+        pass
