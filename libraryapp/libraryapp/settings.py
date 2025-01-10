82158e65b353ad81180114a24b903340b1b6e816
Mohammad <m.shirzady44@gmail.com>
2025-01-11 00:30:50 +0330

    add camma -nothing

--- libraryapp/libraryapp/settings.py
+++ libraryapp/libraryapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libapp.apps.LibappConfig'
+    'libapp.apps.LibappConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
