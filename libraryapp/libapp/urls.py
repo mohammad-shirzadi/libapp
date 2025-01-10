ef288a634955f29799152f2a4bb001b4de7801ea
Mohammad <m.shirzady44@gmail.com>
2025-01-11 00:27:31 +0330

    urls updated api address

--- libraryapp/libapp/urls.py
+++ libraryapp/libapp/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from libapp import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('boroow/<str:borrow>',views.borrow,name='borrow')
+    path('api/login/', views.login, name='login'),
+    path('api/signup/', views.signup, name='signup'),
+    path('api/borrow/',views.borrow,name='borrow'),
+    path('api/returnbook/',views.returnbook,name='returnbook'),
+    path('api/search/', views.search, name='search'),
 ]
