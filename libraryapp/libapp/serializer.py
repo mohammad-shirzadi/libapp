44542e8ce5e25cbd49e4ea9818556112e0d35326
Mohammad <m.shirzady44@gmail.com>
2025-01-15 12:09:52 +0330

    serializer modified to show book and user info

--- libraryapp/libapp/serializer.py
+++ libraryapp/libapp/serializer.py
@@ -5,15 +5,16 @@ from rest_framework import serializers
 class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
 
 class bookSerializer(serializers.ModelSerializer):
     class Meta:
         model = bookModel
         fields = '__all__'
 
-class borrowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = borrowModel
-        fields = '__all__'
-        
+class borrowSerializer(serializers.Serializer):
+    borrowID = serializers.IntegerField()
+    Bbook = bookSerializer()
+    Buser = userSerializer()
+    borrowdate = serializers.DateField()
+    returndate = serializers.DateField()
